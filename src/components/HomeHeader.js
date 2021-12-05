93a5ea16f7bf45e7fda756198e1fe895b6ce18a3
Mela1903 <agnieszka.flak1@gmail.com>
2021-12-05 22:08:22 +0100

    Main page 1b - navigation and main section 1c - 3 columns

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const HomeHeader = () => {
     return (
-        <div>
-                header
+        <div className='home'>
+
+            <div className='home__backgroundImage'></div>
+            <div className='home__open'>
+                <nav>
+                    <ul className="page-nav-actions">
+                        <li><a href="#" className="nav__link">Zaloguj</a></li>
+                        <li><a href="#" className="nav__link">Załóż konto</a></li>
+                    </ul>
+                    <ul className="page-nav-list">
+                        <li><a href="#" className="nav__link">Start</a></li>
+                        <li><a href="#" className="nav__link">O co chodzi?</a></li>
+                        <li><a href="#" className="nav__link">O nas</a></li>
+                        <li><a href="#" className="nav__link">Fundacja i organizacje</a></li>
+                        <li><a href="#" className="nav__link">Kontakt</a></li>
+                    </ul>
+                </nav>
+                <div className='home__text'>
+                    <h1>Zacznij pomagać!<br/>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    <img src={require('../assets/Decoration.svg').default} alt='Decoration' className='decoration'/>
+                    <div className='flex space-around'>
+                        <button className='btn btn__upper'>oddaj <br/>rzeczy</button>
+                        <button className='btn btn__upper'>zorganizuj zbiórkę</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const HomeThreeColumns = () => {
     return (
-        <div>
-            three columns
+        <div className='flex threeColumns'>
+            <div className='box'>
+                <div className='box_value'>10</div>
+                <div className='box_objective'>oddanych worków</div>
+                <div className='box_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
+            </div>
+            <div className='box'>
+                <div className='box_value'>5</div>
+                <div className='box_objective'>wspartych organizacji</div>
+                <div className='box_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
+            </div>
+            <div className='box'>
+                <div className='box_value'>7</div>
+                <div className='box_objective'>zorganizowanych zbiórek</div>
+                <div className='box_description'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
+            </div>
         </div>
     );
 };
