57c63f158e488ecee5deb05f1e9237a0d79aa08a
Mela1903 <agnieszka.flak1@gmail.com>
2021-12-12 22:57:52 +0100

    register validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,29 +8,60 @@ const Register = () => {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const { register } = useAuth()
-    const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Podane hasła są różne')
+    const resetRegistrationErrors = () => {
+        return {
+            email: '',
+            passwordMatch: '',
+            passwordLength: '',
+            cannotCreateAccount: ''
         }
-
-        try {
-            setError('')
-            setLoading(true)
-            await register(emailRef.current.value, passwordRef.current.value)
-            navigate('/')
-        } catch {
-            setError('Nie udało się utworzyć konta')
-        }
-        setLoading(false)
     }
 
-    const redirect = e => {
+    const [error, setError] = useState(resetRegistrationErrors);
+
+    const isEmailValid = (email) => {
+        const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+    }
+
+    const validateRegisterForm = (email, password, passwordConfirm) => {
+        let errorsTemp = {...error};
+        errorsTemp.email = !isEmailValid(email) ? "Niepoprawny email" : "";
+        errorsTemp.passwordLength = password.length < 6 ? "Hasło musi mieć conajmniej 6 znaków" : "";
+        errorsTemp.passwordMatch = password !== passwordConfirm ? "Podane hasła są różne" : "";
+        errorsTemp.cannotCreateAccount = '';
+        setError({...errorsTemp});
+        return Object.values(errorsTemp).every(x => x === "");
+    }
+
+    const renderEmailError = () => error.email && <p className='field-error'>{error.email}</p>
+    const renderPasswordError = () => error.passwordLength && <p className='field-error'>{error.passwordLength}</p>
+    const renderPasswordConfirmError = () => error.passwordMatch && <p className='field-error'>{error.passwordMatch}</p>
+    const renderAPIError = () => error.cannotCreateAccount && <p className='field-error'>{error.cannotCreateAccount}</p>
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const passwordConfirm = passwordConfirmRef.current.value;
+
+        if (validateRegisterForm(email, password, passwordConfirm)) {
+            try {
+                resetRegistrationErrors()
+                setLoading(true)
+                await register(email, password)
+                navigate('/')
+            } catch {
+                setError(prevState => ({...prevState, cannotCreateAccount: `konto dla adresu ${email} już istnieje`}));
+            }
+            setLoading(false)
+        }
+    }
+
+    const redirectToLogin = e => {
         e.preventDefault()
         navigate('/logowanie')
     }
@@ -44,26 +75,51 @@ const Register = () => {
                 <h2 className='headers_text'>Zaloguj się</h2>
                 <img src={require('../assets/Decoration.svg').default} alt='Decoration'/>
 
-                <form className='form' onSubmit={handleSubmit}>
+                <form className='form' onSubmit={handleSubmit} id='register-form'>
                     <div className='form__fields'>
                         <div className='form__field'>
                             <label className='form__label'>Email</label>
-                            <input className='form__input' type="mail" name="name" ref={emailRef}/>
+                            <input
+                                className={(error.email) ? 'form__input error' : 'form__input'}
+                                type="mail"
+                                ref={emailRef}
+                                required
+                            />
+                            {renderEmailError()}
                         </div>
                         <div className='form__field'>
                             <label className='form__label form__label-last'>Hasło</label>
-                            <input className='form__input' type="password" name="password" ref={passwordRef}/>
+                            <input
+                                className={(error.passwordLength) ? 'form__input error' : 'form__input'}
+                                type="password"
+                                ref={passwordRef}
+                                required
+                            />
+                            {renderPasswordError()}
                         </div>
                         <div className='form__field'>
                             <label className='form__label form__label-last'>Powtórz hasło</label>
-                            <input className='form__input' type="password" name="passwordConfirm" ref={passwordConfirmRef}/>
+                            <input
+                                className={(error.passwordLength || error.passwordMatch) ? 'form__input error' : 'form__input'}
+                                type="password"
+                                ref={passwordConfirmRef}
+                                required
+                            />
+                            {renderPasswordConfirmError()}
                         </div>
                     </div>
                     <div className='form__links'>
-                        <button className='btn btn__login' onClick={redirect}>Zaloguj się</button>
-                        <button className='btn btn__login' disabled={loading} type="submit">Załóż konto</button>
+                        <button className='btn btn__login' onClick={redirectToLogin}>Zaloguj się</button>
+                        <button
+                            className='btn btn__login'
+                            disabled={loading}
+                            type="submit"
+                            form='register-form'
+                        >
+                            Załóż konto</button>
                     </div>
                 </form>
+                {renderAPIError()}
             </div>
         </div>
     );
